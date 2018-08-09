782910e04b57437a1b55733fd9a60c388dce826b
temy13 <temy.poporofier00@gmail.com>
2018-08-09 19:52:31 +0900

    action rm

--- db/migrate/20180323134609_add_columns_to_log.rb
+++ db/migrate/20180323134609_add_columns_to_log.rb
@@ -1,7 +1,7 @@
 class AddColumnsToLog < ActiveRecord::Migration[5.1]
   def change
-    add_column :action_logs, :remote_ip, :string
-    add_column :action_logs, :ip, :string
+    # add_column :action_logs, :remote_ip, :string
+    # add_column :action_logs, :ip, :string
     add_column :users, :score, :integer, :default => 0
 
 
