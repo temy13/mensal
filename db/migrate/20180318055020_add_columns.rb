4deac06aa0452dc4a5ac10f697c1d4e93eed2d90
temy13 <temy.poporofier00@gmail.com>
2018-08-09 18:56:18 +0900

    db set

--- db/migrate/20180318055020_add_columns.rb
+++ db/migrate/20180318055020_add_columns.rb
@@ -1,7 +1,7 @@
 class AddColumns < ActiveRecord::Migration[5.1]
   def change
-    add_column :books, :subtitle, :string
-    add_column :books, :google_categories, :string
+    # add_column :books, :subtitle, :string
+    # add_column :books, :google_categories, :string
 
     add_column :questions, :score, :integer, default: 0
     add_column :answers, :score, :integer, default: 0
--- db/migrate/20180322093036_add_affiliate_url2.rb
+++ db/migrate/20180322093036_add_affiliate_url2.rb
@@ -1,5 +1,5 @@
 class AddAffiliateUrl2 < ActiveRecord::Migration[5.1]
   def change
-    add_column :books, :rakuten_affiliate_url, :string
+    # add_column :books, :rakuten_affiliate_url, :string
   end
 end
